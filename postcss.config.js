2db803a73a8e123c9ba974d347b9084b798d3dd9
johann <xperienceweb324@gmail.com>
2022-09-11 22:58:20 -0500

    visto hasta el video 9

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    Plugin: [
+            require("tailwindcss"),
+            require("autoprefixer"),
+    ]
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily: {
+      display: ["Gilroy", "sans.serif"],
+      body: ["graphik", "sans.serif"]
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
